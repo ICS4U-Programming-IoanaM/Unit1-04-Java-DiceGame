3c7953562b8f35079799f051898e8b5bdb7adaf9
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-03-04 22:39:37 +0000

    Initial push + package import

---
+++ DiceGame.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * This program is a game in which the user has to guess
+ * what the number on a dice is.
+ *
+ * @author Ioana Marinescu
+ * @version 1.0
+ * @since 2024-03-04
+ */
+public final class DiceGame {
+  /** Private constructor to prevent instantiation of this utility class. */
+  private DiceGame() {
+    // this comment makes the linter happy
+  }
+
+  /**
+   * The main method prints a stylish greeting message.
+   *
+   * @param args Command-line arguments (not used in this program)
+   */
+  public static void main(final String[] args) {
+    
+  }
+}
